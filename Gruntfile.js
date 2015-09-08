56224aa4beb772470b4dd7f14d3eb4a28dc212d8
softvar <varun2902@gmail.com>
2015-09-09 01:46:17 +0530

    Gruntfile for diff tasks

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		serve: {
+			options: {
+				port: 9000
+			}
+		},
+		watch: {
+			css: {
+				files: 'src/*.*',
+				tasks: ['build'],
+				options: {
+					livereload: true,
+				}
+			},
+		},
+		uglify: {
+			js: {
+				files: {
+					'dist/game.min.js': ['src/main.js']
+				}
+			}
+		},
+		cssmin: {
+			css: {
+				files: {
+					'dist/game.min.css': ['src/css/game.css']
+				}
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					collapseWhitespace: true
+				},
+				files: {
+					'dist/index.html': 'src/index.html'
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('build', [
+		'uglify',
+		'cssmin',
+		'htmlmin'
+	]);
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-serve');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+};
